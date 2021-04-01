fd0d1ce7fe7fa1dd90626cbaac718488509c4a7f
Dejan85 <xypnotica@gmail.com>
2021-04-01 17:39:29 +0200

    refactore

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,24 +1,16 @@
+import { NumbersCollection } from './NumbersCollection';
+
 export class Sorter {
-    constructor(public collection: number[] | string) { }
+
+    constructor(public collection: NumbersCollection) { }
 
     sort(): void {
         const { length } = this.collection;
 
         for (let i: number = 0; i < length; i++) {
             for (let j: number = 0; j < length - i - 1; j++) {
-
-                // if collection is an array of numbers
-                if (this.collection instanceof Array) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-
-                // if collection is a string, do this logic instead
-                // logic to compare and swap characters in a string
-                if (typeof this.collection === 'string') {
+                if (this.collection.compare(j, j + 1)) {
+                    this.collection.swap(j, j + 1);
                 }
             }
         }
