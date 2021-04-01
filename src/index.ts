2bea6febcc49155ecb57437bcb90c327bbef4f68
Dejan85 <xypnotica@gmail.com>
2021-04-01 21:37:50 +0200

    added character collection

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharacterCollection } from './CharacterCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
+const characterCollection = new CharacterCollection('testw');
+
+const sorter = new Sorter(characterCollection);
 sorter.sort();
 
 console.log('test', numbersCollection.data);
+console.log('test', characterCollection.data);
+
 
